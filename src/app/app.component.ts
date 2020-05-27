0fab2582a822939b5120f5255c62a2fcafb3dce4
rafik taamma <b.taamma@esi-sba.dz>
2020-05-27 06:46:54 +0100

    second commit fixing some bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { HostListener } from '@angular/core';
+import {BreakpointObserver, BreakpointState , Breakpoints} from '@angular/cdk/layout';
 
 
 
@@ -12,8 +13,13 @@ export class AppComponent implements OnInit {
   public ajaxSettings: object;
   public contextMenuSettings: object;
   public toolbarSettings: object;
+  public view: string;
   public allowMultiSelection: boolean;
   public hostUrl = 'https://file-provider-nodejs.herokuapp.com/';
+  constructor(public breakpointObserver: BreakpointObserver) {
+  }
+
+
   public ngOnInit(): void {
     // Initializing the File Manager with NodeJS service.
     this.ajaxSettings = {
@@ -26,13 +32,21 @@ export class AppComponent implements OnInit {
          getImageUrl : this.hostUrl + 'GetImage'
     };
 
+    if (this.breakpointObserver.isMatched('(max-width: 767.98px)'))  {
+      this.view = 'Details';
+    } else {
+      this.view = 'LargeIcons';
+    }
+
+
     this.contextMenuSettings = { file: ['Open', 'Download' , '|', 'Details'],
       folder: ['Open', 'Download' , '|', 'Details'],
       layout: ['SortBy', 'View', 'Refresh', '|', 'Details', '|'],
       visible: true};
     this.toolbarSettings = { items: ['Download', 'SortBy', 'Refresh', 'Selection' , 'View', 'Details'], visible: true};
-    this.allowMultiSelection = false;
+    this.allowMultiSelection = true;
   }
+
 @HostListener('window:scroll', ['$event'])
 onWindowScroll(e) {
   const element = document.querySelector('.navbar');
@@ -45,5 +59,10 @@ onWindowScroll(e) {
     element.classList.remove('py-0');
     element.classList.add('py-lg-3');
   } }
-
+  // NoShow(reachedTarget: boolean): void {
+  //   if (reachedTarget) {
+  //     const element = document.querySelector('#navbarNav');
+  //     element.classList.remove('show');
+  //   }
+  // }
 }
